e0ad92f8fe48a6a5765b1bbe8333877a56d228ed
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2023-03-19 18:05:37 +0300

    feat: create main template

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 
 import { createURL } from './modules/createURL';
 import { renderHeader } from './modules/renderHeader';
+import { renderMain } from './modules/renderMain';
 import en from './localizations/en.json';
 import es from './localizations/es.json';
 import fr from './localizations/fr.json';
@@ -11,10 +12,17 @@ import nl from './localizations/nl.json';
 import ru from './localizations/ru.json';
 import zh from './localizations/zh.json';
 import closeImg from './assets/close.svg';
+import unlimited from './assets/unlimitedDocs.svg';
+import exportArrow from './assets/export.svg';
+import textImg from './assets/noAds.svg';
 
 const HEADER = document.querySelector('.header');
 const MAIN = document.querySelector('.main');
 const FOOTER = document.querySelector('.footer');
+
+const PRICE_MONTH = 9.99;
+const PRICE_YEAR = 19.99;
+const PRICE_DISCOUNT = 1.66;
 
 // const languageArr = ['en', 'es', 'fr', 'ja', 'nl', 'ru', 'zh'];
 const languageArr = ['en', 'es', 'fr', 'ja', 'nl', 'zh'];
@@ -36,24 +44,87 @@ let currentLang = searchLanguage.get('lang');
 switch (currentLang) {
   case 'es':
     HEADER.innerHTML = renderHeader(es, closeImg);
+    MAIN.innerHTML = renderMain(
+      es,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   case 'fr':
     HEADER.innerHTML = renderHeader(fr, closeImg);
+    MAIN.innerHTML = renderMain(
+      fr,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   case 'ja':
     HEADER.innerHTML = renderHeader(ja, closeImg);
+    MAIN.innerHTML = renderMain(
+      ja,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   case 'nl':
     HEADER.innerHTML = renderHeader(nl, closeImg);
+    MAIN.innerHTML = renderMain(
+      nl,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   case 'ru':
     HEADER.innerHTML = renderHeader(ru, closeImg);
+    MAIN.innerHTML = renderMain(
+      ru,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   case 'zh':
     HEADER.innerHTML = renderHeader(zh, closeImg);
+    MAIN.innerHTML = renderMain(
+      zh,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
     break;
   default:
     HEADER.innerHTML = renderHeader(en, closeImg);
+    MAIN.innerHTML = renderMain(
+      en,
+      PRICE_MONTH,
+      PRICE_YEAR,
+      PRICE_DISCOUNT,
+      unlimited,
+      exportArrow,
+      textImg
+    );
 }
 
 // function templ(data, priceMonth, priceYear) {
---
+++ src/modules/renderMain.js
@@ -0,0 +1,68 @@
+export const renderMain = function (
+  data,
+  priceMonth,
+  priceYear,
+  priceMonthDiscount,
+  unlim,
+  exportArrow,
+  text
+) {
+  const templateVariableRe = /\{\{(.*?)\}\}/g;
+  return `
+      <h1 class="main__title">
+        ${data['Unlimited Access<br>to All Features']}
+      </h1>
+      <div class="main__description">
+        <div class="description">
+          <div class="description__img">
+            <img src="${unlim}" alt="unlimited">
+          </div>
+          <div class="description__text">
+             ${data['Unlimited documents']}
+          </div>
+        </div>
+        <div class="description">
+          <div class="description__img">
+            <img src="${exportArrow}" alt="export">
+          </div>
+          <div class="description__text">
+             ${data['Export to clouds']}
+          </div>
+        </div>
+        <div class="description">
+          <div class="description__img">
+            <img src="${text}" alt="text recognition">
+          </div>
+          <div class="description__text">
+             ${data['Text recognition (OCR)']}
+          </div>
+        </div>
+      </div>
+      <div class="main__price">
+        <div class="price price-active">
+          <h3 class="price__title">${data['Monthly']}</h3>
+          <div class="price__value">${data[
+            '<strong>{{price}}</strong><br>per month'
+          ].replace(templateVariableRe, priceMonth)}</div>
+          <div class="price__description">${data['3 DAYS FREE']}</div>
+          <div class="price__value-repeat">${data['{{price}}/month'].replace(
+            templateVariableRe,
+            priceMonth
+          )}</div>
+        </div>
+        <div class="price price__action">
+          <div class="price__discount"></div> 
+          <h3 class="price__title">${data['Annually']}</h3>
+          <div class="price__value">${data[
+            '<strong>{{price}}</strong><br>per year'
+          ].replace(templateVariableRe, priceYear)}</div>
+          <div class="price__description">${data['MOST POPULAR']}</div>
+          <div class="price__value-repeat">${data['{{price}}/month'].replace(
+            templateVariableRe,
+            priceMonthDiscount
+          )}</div>
+        </div>
+      </div>
+      <a class="btn" href="#" >${data['Continue']}</a>
+`;
+};
